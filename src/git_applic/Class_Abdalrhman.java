362d94c3296776a268671c3fb6d1ba026c99c0aa
abdalrhman <abdalrhman@gmail.com>
2026-02-25 00:10:25 +0300

    انشاء باني للخصائص و انشاء دالة طباعة المعلومات المستخدم

--- src/git_applic/Class_Abdalrhman.java
+++ src/git_applic/Class_Abdalrhman.java
@@ -3,4 +3,16 @@ public class Class_Abdalrhman {
     public int Id;
     public String Name;
     public int Age ;
+    public Class_Abdalrhman (int Id , String Name, int Age){
+        this.Id = Id;
+        this.Name = Name;
+        this.Age = Age;
+    }
+    public void PrintInformatino(){
+        System.out.println("The Id Number : "+Id);
+        System.out.println("Name Is : "+Name);
+        System.out.println("Age Is : "+Age);
+    }
+        
+    
 }
--- src/git_applic/Git_applic.java
+++ src/git_applic/Git_applic.java
@@ -1,7 +1,8 @@
 package git_applic;
 public class Git_applic {
     public static void main(String[] args) {
-       Class_Abdalrhman class_abd = new Class_Abdalrhman(); 
+       Class_Abdalrhman class_abd = new Class_Abdalrhman(1,"Abdalrhman",20);
+       class_abd.PrintInformatino();
     }
     
 }
